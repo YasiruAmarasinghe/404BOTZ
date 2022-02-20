15bcc669f594b02ebccacdf8ce85f74b7cfd240a
YasiruAmarasinghe <93648091+YasiruAmarasinghe@users.noreply.github.com>
2022-02-20 11:33:33 +0530

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -1,34 +1,22 @@
-let limit = 130
+let  limit  =  110
 const { servers, yta } = require('../lib/y2mate')
+
 let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
-  if (!args || !args[0]) throw `Example:\n${usedPrefix + command} https://www.youtube.com/watch?v=yxDdj_G9uRY`
-  let chat = global.db.data.chats[m.chat]
+  if (!args || !args[0]) throw `like this:\n${usedPrefix + command} <url>\n\nExample:\n${usedPrefix + command} https://www.youtube.com/watch?v=yxDdj_G9uRY`
+  let chat = db.data.chats[m.chat]
   let server = (args[1] || servers[0]).toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-  m.reply(isLimit ? `File size: ${filesizeF}\nMax file size reached${limit} MB, download it your self: ${dl_link}` : global.wait)
+  let  isLimit  =  ( isPrems  ||  isOwner ? 99 : limit )  *  1024  <  filesize
+  m . reply ( isLimit ? `File Size: ${ filesizeF } \nFile size above ${ limit } MB, download itself: ${ dl_link } ` : wait )
   if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
-*Title:* ${title}
-*File size:* ${filesizeF}
+*Title:* ${ title }
+*File Size:* ${ filesizeF }
 `.trim(), m, null, {
-    asDocument: true, mimetype: 'audio/mp3' //blOK salah satu jika ingin audio ke dokumen
-    //asDocument: chat.useDocument, mimetype: 'audio/mp4'
-  })
+    asDocument: chat.useDocument, mimetype: 'audio/mp3'
+  } )
 }
 handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <url> [server: ${servers.join(', ')}]`)
 handler.tags = ['downloader']
-handler.command = /^yt(a(udio)?|mp3)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
+handler.command = /^yt(a|mp3)$/i
 
 module.exports = handler
-
