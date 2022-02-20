94121b796ada9fa7ad502bcf30ea60c601fdb1cc
YasiruAmarasinghe <93648091+YasiruAmarasinghe@users.noreply.github.com>
2022-02-20 11:35:21 +0530

    Create update1.js

---
+++ plugins/update1.js
@@ -0,0 +1,24 @@
+let { execSync } = require('child_process')
+let handler = async (m, { conn, text }) => {
+  if (global.conn.user.jid == conn.user.jid) {
+    let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
+    require('fs').readdirSync('plugins').map(v=>global.reload('', v))
+    conn.reply(m.chat, stdout.toString(), m)
+  }
+}
+handler.help = ['updatey']
+handler.tags = ['host']
+handler.command = /^updatey$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+
+module.exports = handler
