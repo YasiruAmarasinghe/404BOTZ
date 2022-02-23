87cbca6ccfdd6af7dd5e9938970a7ef01920a981
YasiruAmarasinghe <93648091+YasiruAmarasinghe@users.noreply.github.com>
2022-02-23 14:57:18 +0530

    Update ytdl.js

--- plugins/ytdl.js
+++ plugins/ytdl.js
@@ -1,19 +1,19 @@
-let limit = 120
+let limit = 130
 let fetch = require('node-fetch')
 const { servers, yt } = require('../lib/y2mate')
 
-let handler = async (m, { itsu, args, isPrems, isOwner, usedPrefix, text }) => {
-  if (!args || !args[0]) throw 'Uhm... Where URL?'
-  let chat = global.DATABASE.data.chats[m.chat]
+let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, text }) => {
+  if (!args || !args[0]) throw 'Uhm... where url?'
+  let chat = global.db.data.chats[m.chat]
   let quality = args[1] || '360'
   let server = (args[2] || servers[0]).toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF} = await yt(args[0], quality + 'p', 'mp4', quality, servers.includes(server) ? server : servers[0])
   
   let isLimit = (isPrems || isOwner ? 99 : limit) * 998888 < filesize
-await itsu.relayWAMessage(itsu.prepareMessageFromContent(m.chat, {
+await conn.relayWAMessage(conn.prepareMessageFromContent(m.chat, {
                     "listMessage":  {
                         "title": `*Title*: ${title}`,
-                        "description": " \n_©Yasiru_",
+                        "description": " \n 404 WhatsApp Bot ",
                         "buttonText": `Quality`,
                         "listType": "SINGLE_SELECT",
                         "sections": [
@@ -23,27 +23,27 @@ await itsu.relayWAMessage(itsu.prepareMessageFromContent(m.chat, {
                                         "title": `Audio`, "description":  title,
                                         "rowId": `${usedPrefix}dlmsc ${args[0]}`
                                     }, {
-                                       "title": `240p✅ `, "description": title,
+                                       "title": `240p` , "description": title,
                                        "rowId": `${usedPrefix}dlvid ${args[0]} 240`
                                     }, {
-                                       "title": `360p✅`,
+                                       "title": `360p`,
 "description": title, 
                                        "rowId": `${usedPrefix}dlvid ${args[0]} 360`
                                     }, {
-                                       "title": `480p✅`,
+                                       "title": `480p`,
 "description": title, 
                                         "rowId": `${usedPrefix}dlvid ${args[0]} 480`
                                     }, {
-                                        "title": `720p✅`,
+                                        "title": `720p`,
 "description": title, 
                                         "rowId": `${usedPrefix}dlvid ${args[0]} 720`                               
                                     }, { 
-                                        "title": `1080p✅`,
+                                        "title": `1080p`,
 "description":  title, 
                                         "rowId": `${usedPrefix}dlvid ${args[0]} 1080`
                                     },{
 "title": `Menu`,
-"description": 'bot menu', 
+"description": ``, 
                                        "rowId": `${usedPrefix} menu`}
                                 ]
                             }
@@ -54,10 +54,12 @@ await itsu.relayWAMessage(itsu.prepareMessageFromContent(m.chat, {
 }
 handler.help = ['ytdl ']
 handler.tags = ['downloader']
-handler.command = /^(ytdl|yt)$/i
+handler.command = /^ytdl$/i
 
 module.exports = handler
 
 async function shortUrl(url) {
   return await (await fetch('https://tinyurl.com/api-create.php?url=' + encodeURIComponent(url))).text()
 }
+
+//Sc Github.com/Rlxfly
