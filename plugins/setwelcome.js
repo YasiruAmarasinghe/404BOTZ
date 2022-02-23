eb3dca9c02fc063944b31bbcd2c031688b604e84
YasiruAmarasinghe <93648091+YasiruAmarasinghe@users.noreply.github.com>
2022-02-23 14:55:06 +0530

    Update setwelcome.js

--- plugins/setwelcome.js
+++ plugins/setwelcome.js
@@ -6,9 +6,8 @@ let handler = async (m, { conn, text, isROwner, isOwner, usedPrefix, command }) 
     m.reply('Welcome set successfully\n@user (Mention)\n@subject (Title Grup)\n@desc (Description Grup)')
   } else throw `uhm.. where's the text?\n\nExample:\n${usedPrefix + command} welcome @user group @subject\n\n@desc`
 }
-handler.help = ['setwelcome <teks>']
+handler.help = ['setwelcome <text>']
 handler.tags = ['owner', 'group']
 
 handler.command = /^setwelcome$/i
 module.exports = handler
-
