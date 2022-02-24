f08de1c2c71dddc7242cc6eda2f90bed637b0158
YasiruAmarasinghe <93648091+YasiruAmarasinghe@users.noreply.github.com>
2022-02-24 16:14:23 +0530

    Update emomix.js

--- plugins/emomix.js
+++ plugins/emomix.js
@@ -7,7 +7,7 @@ if (!args[0]) throw 'EXAMPLE:\n\n*.emomix 🐷.😣* or *.emojimix 🤡&👽* '
    	let [emoji1, emoji2] = text.split`&`
 		let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
 		for (let res of anu.results) {
-		    let encmedia = await conn.sendImageAsSticker(m.chat, res.url, m, { packname: global.packname, author: global.author, categories: res.tags })
+		    let encmedia = await this.sendImageAsSticker(m.chat, res.url, m, { packname: global.packname, author: global.author, categories: res.tags })
 		    await fs.unlinkSync(encmedia)
 		}
 }
