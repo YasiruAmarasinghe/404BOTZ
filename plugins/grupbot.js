4497b28b785fd81ecb3102d8769b45c2c9b8d204
Yasiru  Amarasinghe <93648091+YasiruAmarasinghe@users.noreply.github.com>
2022-03-10 15:33:59 +0530

    Update grupbot.js

--- plugins/grupbot.js
+++ plugins/grupbot.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 *Grup Official Forum Bot*
 
-https://chat.whatsapp.com/IZcDaQwOYlR6cFLeol8uYd
+https://chat.whatsapp.com/EcDf9Dsm5LKDY6ysSnsxZ4
 `.trim())
 handler.help = ['groupbot']
 handler.tags = ['info', 'grup']
