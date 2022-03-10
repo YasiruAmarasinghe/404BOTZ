2775ff1095a9e5799c2ea0b3353e73ae55225be0
Yasiru  Amarasinghe <93648091+YasiruAmarasinghe@users.noreply.github.com>
2022-03-10 15:33:25 +0530

    Update Alive.js

--- plugins/Alive.js
+++ plugins/Alive.js
@@ -1,7 +1,7 @@
 let fetch = require ('node-fetch')
 let handler = async (m, { conn }) => {
       await conn.send2ButtonLoc(m.chat, await (await fetch(image)).buffer(), `
-Hey there, 404Bot online now Type .menu to get my full command list \n\nGroup official bot forum:https://chat.whatsapp.com/IZcDaQwOYlR6cFLeol8uYd\nThankyou for using 404BOT™
+Hey there, 404Bot online now Type .menu to get my full command list \n\nGroup official bot forum:https://chat.whatsapp.com/EcDf9Dsm5LKDY6ysSnsxZ4\nThankyou for using 404BOT™
 `.trim(), watermark, 'Menu', `.menu`, 'Owner', `.owner`)
 }
 handler.tags = ['main']
