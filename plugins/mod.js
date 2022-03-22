4c108c10c9abd41ab844bb0894beee7ed3a95aab
Yasiru  Amarasinghe <93648091+YasiruAmarasinghe@users.noreply.github.com>
2022-03-22 22:09:23 +0530

    Update mod.js

--- plugins/mod.js
+++ plugins/mod.js
@@ -1,13 +1,13 @@
 let fetch = require('node-fetch')
 
-let handler = async (m, { conn, args }) => {
+let handler = async (m, { conn, args, text }) => {
  if (!args[0]) throw 'Uhm.. please give a URL?'
  let res = await fetch(API('apisanuwa', '/docs/download/rexdl', { url: args[0] }, 'apikey'))
  if (!res.ok) throw await res.text()
  let json = await res.json()
- let { title, url } = json.download
- m.reply(JSON.stringify(json.download, null, 2))
- conn.sendFile(m.chat, url, title, '', m)
+ let { title, url } = json.data[0]
+ 
+  await conn.sendFile(m.chat, url, title, null, m)
 }
 handler.help = ['mod'].map(v => v + ' <url>')
 handler.tags = ['downloader']
